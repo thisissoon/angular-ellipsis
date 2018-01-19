ff7eedf821bc4cb47f6b96394ba5e4b37d222008
Matheus Araujo <matheus.araujo@e-strident.com>
2018-01-19 13:53:35 +0100

    fix(ellipsis): add support for dynamic content
    Before it was clipping the text OnInit, in order to support dynamic content, now it uses AfterViewInit
    
    Also add examples for different ways of using it.

--- src/app/ellipsis/ellipsis.directive.spec.ts
+++ src/app/ellipsis/ellipsis.directive.spec.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 import { EllipsisDirective } from './ellipsis.directive';
 
 // tslint:disable-next-line:max-line-length
-const template = `<p snEllipsis>Ullamco esse laborum dolor eiusmod laboris aliquip aute aute aute. Ullamco velit ad laboris consequat. Deserunt ad reprehenderit cupidatat do labore esse. Occaecat nostrud mollit commodo ut ex elit fugiat et reprehenderit quis. Fugiat aliquip excepteur quis sunt sint consectetur duis elit quis ex fugiat quis eiusmod. Ad pariatur ipsum nulla sunt est non ut id et nisi culpa voluptate mollit ad. Tempor cupidatat esse ad in cillum incididunt quis. Nulla cillum qui aute labore quis ad cillum ullamco adipisicing qui est nulla amet. Nisi consectetur sint nostrud est duis dolore enim aliqua esse laboris Lorem.</p>`;
+const baseContent = `Ullamco esse laborum dolor eiusmod laboris aliquip aute aute aute. Ullamco velit ad laboris consequat. Deserunt ad reprehenderit cupidatat do labore esse. Occaecat nostrud mollit commodo ut ex elit fugiat et reprehenderit quis. Fugiat aliquip excepteur quis sunt sint consectetur duis elit quis ex fugiat quis eiusmod. Ad pariatur ipsum nulla sunt est non ut id et nisi culpa voluptate mollit ad. Tempor cupidatat esse ad in cillum incididunt quis. Nulla cillum qui aute labore quis ad cillum ullamco adipisicing qui est nulla amet. Nisi consectetur sint nostrud est duis dolore enim aliqua esse laboris Lorem.`;
+const trimmedContent = `Ullamco esse laborum dolor eiusmod laboris aliquip aute aute aute. Ullamco velit ad laboris consequat. Deserunt ad reprehenderit cupidatat do labore esse. Occaecat nostrud mollit commodo ut ex elit fugiat et reprehenderit quis. Fugiat aliquip excepteur quis sunt sint consectetur duis elit…`;
+
+const template = `
+  <p class="static" snEllipsis>${baseContent}</p> \
+  <p class="dynamic" snEllipsis>{{ dynamicContent }}</p> \
+  <p class="html" snEllipsis [innerHTML]="htmlContent"></p>
+`;
 
 @Component({
   selector: 'sn-test-component',
@@ -13,10 +20,14 @@ const template = `<p snEllipsis>Ullamco esse laborum dolor eiusmod laboris aliqu
     p { height: 200px; overflow: hidden; padding: 1rem; width: 200px; }
   `]
 })
-class TestComponent { }
+class TestComponent {
+  public dynamicContent: string;
+  public htmlContent: string;
+}
 
-describe('AppComponent', () => {
+describe('EllipsisDirective', () => {
   let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
   let compiled: HTMLElement;
 
   beforeEach(async(() => {
@@ -30,13 +41,30 @@ describe('AppComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+
+    component.dynamicContent = baseContent;
+    component.htmlContent = baseContent.split('.').join('. <br><br>');
+
     compiled = fixture.debugElement.nativeElement;
   });
 
-  it('should render directive and clip text', async(() => {
+  it('should clip static text', async(() => {
     fixture.detectChanges();
-    expect(compiled.querySelector('p').textContent)
+    expect(compiled.querySelector('.static').textContent)
+      .toEqual(trimmedContent);
+  }));
+
+  it('should clip dynamic text', async(() => {
+    fixture.detectChanges();
+    expect(compiled.querySelector('.dynamic').textContent)
+      .toEqual(trimmedContent);
+  }));
+
+  it('should clip HTML text', async(() => {
+    fixture.detectChanges();
+    expect(compiled.querySelector('.html').textContent)
       // tslint:disable-next-line:max-line-length
-      .toEqual(`Ullamco esse laborum dolor eiusmod laboris aliquip aute aute aute. Ullamco velit ad laboris consequat. Deserunt ad reprehenderit cupidatat do labore esse. Occaecat nostrud mollit commodo ut ex elit fugiat et reprehenderit quis. Fugiat aliquip excepteur quis sunt sint consectetur duis elit…`);
+      .toEqual(`Ullamco esse laborum dolor eiusmod laboris aliquip aute aute aute. Ullamco velit ad laboris consequat. Deserunt ad reprehenderit cupidatat do labore esse. Occaecat nostrud mollit…`);
   }));
 });
