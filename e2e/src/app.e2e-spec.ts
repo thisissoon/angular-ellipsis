458994649b9a0e00ce664df26625a67c3eeb7f59
Ed Opare-Aryee <ed@thisissoon.com>
2019-02-06 10:00:58 +0000

    chore: update dependancies and add support for angular 7 and above

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { AppPage } from './app.po';
+import { browser } from 'protractor';
+
+describe('Ellipsis Lib E2E Tests', function() {
+  let page: AppPage;
+
+  beforeEach(() => (page = new AppPage()));
+
+  beforeEach(() => page.navigateTo());
+
+  afterEach(() => {
+    browser
+      .manage()
+      .logs()
+      .get('browser')
+      .then((browserLog: any[]) => {
+        expect(browserLog).toEqual([]);
+      });
+  });
+
+  describe('clip text', () => {
+    it('should clip text and add ellipsis symbol', () => {
+      // tslint:disable-next-line:max-line-length
+      expect(page.getDefaultParagraphText()).toEqual(
+        `Ullamco esse laborum dolor eiusmod laboris aliquip aute aute aute. Ullamco velit ad laboris consequat. Deserunt ad reprehenderit cupidatat do labore esse. Occaecat nostrud mollit commodo ut ex elit fugiat et reprehenderit quis. Fugiat aliquip excepteur quis sunt sint consectetur duis elit…`
+      );
+    });
+
+    it('should clip text and not loop infinitly', () => {
+      expect(page.getTooSmallParagraphText()).toEqual(`…`);
+    });
+  });
+});
